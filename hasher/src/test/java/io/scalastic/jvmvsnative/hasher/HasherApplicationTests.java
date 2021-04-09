14ae6eb331408644c08835aab8f1bcdba7170640
jeanjerome <contact@scalastic.io>
2021-04-09 16:57:11 +0200

    Enhances java app compilation 🧂

--- hasher/src/test/java/io/scalastic/jvmvsnative/hasher/HasherApplicationTests.java
+++ hasher/src/test/java/io/scalastic/jvmvsnative/hasher/HasherApplicationTests.java
@@ -3,7 +3,7 @@ package io.scalastic.jvmvsnative.hasher;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HasherApplicationTests {
   
   @Test
